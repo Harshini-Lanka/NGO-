0881a2bce58814b98a024b0ba4237db6a8c941e5
Harshini-Lanka <harshinilanka07@gmail.com>
2026-07-09 01:54:58 +0530

    Fix API base URL for production

--- NGO-/src/api/axios.js
+++ NGO-/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://ngo-uoip.onrender.com/",
+  baseURL: "https://ngo-uoip.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
